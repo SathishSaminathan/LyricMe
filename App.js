eecd60a39afbce4cadabcb325167c354d9a7c0f1
SathishSaminathan <Sathishcse1975@gmail.com>
2019-03-29 12:49:53 +0530

    initial_commit

--- App.js
+++ App.js
@@ -6,35 +6,36 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import LyricScreen from "./src/screens/LyricScreen";
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-      </View>
-    );
+    return <LyricScreen />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
---
+++ src/assets/styles/index.js
@@ -0,0 +1,4 @@
+import Colors from "./Colors";
+import textStyles from "./textStyles";
+
+export { Colors,textStyles };
